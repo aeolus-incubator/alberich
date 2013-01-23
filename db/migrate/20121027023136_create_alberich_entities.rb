d9e344f4921053f268b00a0fbf170c828c65c283
Scott Seago <sseago@redhat.com>
2013-01-23 16:03:35 -0500

    fixed migration to work even if user/group migrations have not yet run

--- db/migrate/20121027023136_create_alberich_entities.rb
+++ db/migrate/20121027023136_create_alberich_entities.rb
@@ -7,18 +7,22 @@ class CreateAlberichEntities < ActiveRecord::Migration
       t.integer :lock_version, :default => 0
       t.timestamps
     end
-    Alberich.user_class.constantize.all.each do |u|
-      unless u.entity
-        entity = Entity.new(:entity_target => u)
-        entity.name = u.to_s
-        entity.save!
+    if Alberich.user_class.constantize.table_exists?
+      Alberich.user_class.constantize.all.each do |u|
+        unless u.entity
+          entity = Entity.new(:entity_target => u)
+          entity.name = u.to_s
+          entity.save!
+        end
       end
     end
-    Alberich.user_group_class.constantize.all.each do |ug|
-      unless ug.entity
-        entity = Entity.new(:entity_target => ug)
-        entity.name = ug.to_s
-        entity.save!
+    if Alberich.user_group_class.constantize.table_exists?
+      Alberich.user_group_class.constantize.all.each do |ug|
+        unless ug.entity
+          entity = Entity.new(:entity_target => ug)
+          entity.name = ug.to_s
+          entity.save!
+        end
       end
     end
   end
